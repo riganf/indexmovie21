a3b9ecf4f85d293284d1ade7807304cb3f557164
riganf <34409870+riganf@users.noreply.github.com>
2017-12-27 17:14:14 +0700

    Update kompisafelink3.js

--- kompisafelink3.js
+++ kompisafelink3.js
@@ -25,5 +25,5 @@ if(a_to_vk)
 {a_to_vk.innerHTML+=a_to_va;}}
 function a_to_fa()
 {var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
-protected_links = "im21,net,blogspot.com,youtube.com,facebook.com,google.com,blogger.com,oload.stream,streamango.com";
+protected_links = "im21,net,blogspot.com,youtube.com,facebook.com,google.com,blogger.com,oload.stream,streamango.com,openload.co";
 auto_safelink();
